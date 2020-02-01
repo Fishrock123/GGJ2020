1956013be19cd437caaf181e4a3ff4225e0d1346
avelkash <affonso@gmail.com>
2020-02-01 10:31:11 -0800

    bad thought movement

---
+++ Assets/Scripts/BadThought.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadThought : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 0.001f;
+    private Vector3 directionToTarget;
+    void Start()
+    {
+        directionToTarget = target.position - transform.position;
+        directionToTarget = directionToTarget.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(directionToTarget * speed * Time.deltaTime);
+    }
+}
