37982dc94454546dc3d5ed5aae0cd226b34fbe59
avelkash <affonso@gmail.com>
2020-02-01 01:23:17 -0800

    first behavior draft for good thoughts

---
+++ Assets/Scripts/GoodThought.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodThought : MonoBehaviour
+{
+    public Transform origin;
+    
+    public float rotation = 0; // degrees around origin
+    public float radius = 6;
+
+    public float speed = 0.1f;
+    public float speedToCenter = 0.1f;
+    void Start()
+    {
+        
+        
+    }
+
+    void Update()
+    {
+        radius = Mathf.Lerp(radius, 0, speedToCenter*Time.deltaTime);
+        Vector3 left = new Vector3(origin.position.x - radius, origin.position.y, origin.position.z);
+        Vector3 right = new Vector3(origin.position.x + radius, origin.position.y, origin.position.z);
+        rotation += Time.deltaTime * speed;
+        rotation = rotation % 2;
+
+        Vector3 xform;
+        if (rotation > 1)
+        {
+            xform = Vector3.Slerp(right, left, rotation - 1);
+        }
+        else
+        {
+            xform = Vector3.Slerp(left, right, rotation);
+        }
+
+        if (rotation > 1)
+        {
+            transform.position = new Vector3(xform.x, -xform.z, xform.y);
+        }
+        else
+        {
+            transform.position = new Vector3(xform.x, xform.z, xform.y);
+        }
+    }
+}
