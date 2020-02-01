96fc083eeb14d402f7bdc5313dcae05035bb9ea4
avelkash <affonso@gmail.com>
2020-02-01 12:39:33 -0800

    badThoughts collision and back to the pool

---
+++ Assets/Scripts/BadThoughtCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BadThoughtCollision : MonoBehaviour
+{
+    [System.Serializable]
+    public class BadThoughtEvent : UnityEvent<GameObject> { };
+   
+    public BadThoughtEvent badThoughtEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("FACE"))
+        {
+            badThoughtEvent.Invoke(gameObject);
+        }
+    }
+}
