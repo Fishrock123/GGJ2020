0c20ef8bcdbecb345271be452fcf570db35879f3
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2020-02-02 11:29:26 -0800

    FinalScene: add credits

---
+++ Assets/Scripts/Management/FinalSceneManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalSceneManager : MonoBehaviour
+{
+    Color faded = Color.clear;
+
+    public Image congrats;
+    public Image blackfade;
+    public Image credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fadeCongratsToCredits());
+    }
+
+    IEnumerator fadeCongratsToCredits()
+    {
+        congrats.gameObject.SetActive(true);
+        yield return fadeImageIn(congrats, 2);
+        yield return new WaitForSeconds(5);
+        yield return fadeImageOut(congrats, 2);
+        congrats.gameObject.SetActive(true);
+        credits.gameObject.SetActive(true);
+        yield return fadeImageOut(blackfade, 2);
+    }
+
+    IEnumerator fadeImageIn(Image image, float seconds)
+    {
+        Color originalColor = image.color;
+        image.color = faded;
+
+        float elapsed = 0;
+        while (elapsed < seconds)
+        {
+            image.color = Color.Lerp(faded, originalColor, elapsed / seconds);
+            yield return new WaitForSeconds(Time.deltaTime);
+            elapsed += Time.deltaTime;
+        }
+    }
+
+    IEnumerator fadeImageOut(Image image, float seconds)
+    {
+        Color originalColor = image.color;
+
+        float elapsed = 0;
+        while (elapsed < seconds)
+        {
+            image.color = Color.Lerp(originalColor, faded, elapsed / seconds);
+            yield return new WaitForSeconds(Time.deltaTime);
+            elapsed += Time.deltaTime;
+        }
+
+        image.color = faded;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
