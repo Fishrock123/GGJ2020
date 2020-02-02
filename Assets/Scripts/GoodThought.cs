59938e45193df30e0f1814fb93c0c35dc9c05c3b
avelkash <affonso@gmail.com>
2020-02-01 19:02:33 -0800

    good thoughts rework

--- Assets/Scripts/GoodThought.cs
+++ Assets/Scripts/GoodThought.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GoodThought : MonoBehaviour
 {
-    public Vector3 origin;
+    public Transform origin;
     public float rotation = 0; // degrees around origin
     public float radius = 6;
 
@@ -19,16 +19,23 @@ public class GoodThought : MonoBehaviour
 
     public Transform attachmentFront;
     public Transform attachmentBack;
-    // Start is called before the first frame update
+
     public Transform targetAnchor;
 
     public GameObject trainGFX;
+    public Vector3 left;
+    public Vector3 right;
 
     public AudioSource attachSFX;
     public AudioSource detatchSFX;
 
-    void Start()
+    private void OnEnable()
     {
+        left = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 distanceToCenter = left - Vector3.zero;
+        radius = distanceToCenter.x;
+        //dirToCenter.Normalize();
+        right = new Vector3(distanceToCenter.x*-1, distanceToCenter.y*-1, transform.position.z);
     }
 
     void FixedUpdate()
@@ -41,8 +48,7 @@ public class GoodThought : MonoBehaviour
         }
 
         radius = Mathf.Lerp(radius, 0, speedToCenter*Time.deltaTime);
-        Vector3 left = new Vector3(origin.x - radius, origin.y, origin.z);
-        Vector3 right = new Vector3(origin.x + radius, origin.y, origin.z);
+        
         rotation += Time.deltaTime * speed;
         rotation = rotation % 2;
 
@@ -50,18 +56,11 @@ public class GoodThought : MonoBehaviour
         if (rotation > 1)
         {
             xform = Vector3.Slerp(right, left, rotation - 1);
-        }
-        else
-        {
-            xform = Vector3.Slerp(left, right, rotation);
-        }
-
-        if (rotation > 1)
-        {
             transform.position = new Vector3(xform.x, -xform.z, xform.y);
         }
         else
         {
+            xform = Vector3.Slerp(left, right, rotation);
             transform.position = new Vector3(xform.x, xform.z, xform.y);
         }
     }
