faf66ad01f856f7c5c1c5f259d5d31105a7e006e
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2020-02-01 21:51:46 -0800

    Game: IntroScene, TutorialScene

---
+++ Assets/Scripts/Management/IntroSceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSceneManager : MonoBehaviour
+{
+    GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+
+        if (gameManager == null)
+        {
+            Debug.LogAssertion("Game Manager was not loaded in Intro Scene!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadScene(string name)
+    {
+        gameManager.SetLevel(name);
+    }
+}
---
+++ Assets/Scripts/Tutorial/GoodThoughtTutorial.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodThoughtTutorial : MonoBehaviour
+{
+    public GoodThought goodThought;
+
+    public Transform origin;
+
+    public Transform pos;
+
+    public int attachedFrames = 60;
+    int frames = 0;
+    GoodThought thought;
+
+    public GameObject nextTutorial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        thought = GameObject.Instantiate(goodThought, pos.position, Quaternion.identity);
+        thought.origin = origin;
+        thought.radius = 6;
+        thought.speed = 0.5f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (thought.attached)
+        {
+            frames++;
+        }
+        if (frames > attachedFrames)
+        {
+
+            Destroy(thought.gameObject);
+
+            if (nextTutorial)
+            {
+                nextTutorial.SetActive(true);
+                gameObject.SetActive(false);
+            } else {
+                GameObject.FindObjectOfType<GameManager>().SetLevel("IntroScene");
+            }
+        }
+    }
+}
