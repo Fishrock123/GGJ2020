cf58285bdea3f93881c1a596b363a301a0dd71ac
avelkash <affonso@gmail.com>
2020-02-02 12:47:36 -0800

    creditScene

---
+++ Assets/Scripts/Management/CreditSceneManagement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditSceneManagement : MonoBehaviour
+{
+    GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+
+        if (gameManager == null)
+        {
+            Debug.LogError("Game Manager was not loaded in for Main Menu Button!");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            gameManager.SetLevel("IntroScene");
+        }
+    }
+}
